666fbc1af74959444b53698525056529db204713
Justin Xu and Margie Nevarez <margie@>
2016-10-17 16:02:55 -0700

    made fouth function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 var sentence = prompt("please enter a sentence:");
-
 var firstLetter = sentence.charAt(0);
 var lastLetter = sentence.slice(-1);
-
 var newSentence = function(firstLetter, lastLetter) {
-  return (firstLetter + lastLetter).toUpperCase();
-}
+  return upper = (firstLetter + lastLetter).toUpperCase();
+};
+alert(newSentence(firstLetter,lastLetter));
 
-var finalSentence = alert (newSentence(firstLetter, lastLetter));
+
+
+var combineReverse = function(upper) {
+  return newResults =upper.split('').reverse().join('');
+};
+alert(combineReverse(upper));
+
+
+var results = function (newResults){
+  return finalResults = sentence + newResults;
+};
+alert(results(newResults));
+
+var count = function (sentence){
+  return sentence.charAt((sentence.length)/2) + finalResults;
+};
+
+alert(count(sentence));
